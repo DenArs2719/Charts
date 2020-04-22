dede5c4395d0391948eee5f208c4057456e36934
DenArs2719 <yevseyenkod@mail.ru>
2020-04-22 10:39:39 +0200

    Stworzenie projektu, podstawowe użycie komponentu do rysowania wykresów (Chart, Series)

---
+++ Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Charts
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        public Form()
+        {
+            InitializeComponent();
+
+            Series s = new Series();
+            s.Name = "My function";
+
+            s.ChartType = SeriesChartType.Line; ///typ wykresu
+
+            for(double x = -10;x<=10;x+=0.1)
+            { 
+                s.Points.AddXY(x,x*x); ///dodajemy do wykresu punkty x i naszę funkcję f(x) = x^2   
+            }
+
+            chart.Series.Add(s);
+
+        }
+
+       
+    }
+}
